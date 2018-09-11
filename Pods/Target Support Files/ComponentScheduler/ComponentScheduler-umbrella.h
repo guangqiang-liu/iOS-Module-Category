8987ccd8f8778721ea636bf4a8aece47fd55b04a
刘光强 <liuguangqiang@qianbaocard.com>
2018-09-11 21:10:02 +0800

    添加分类文件

---
+++ Pods/Target Support Files/ComponentScheduler/ComponentScheduler-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ComponentScheduler.h"
+
+FOUNDATION_EXPORT double ComponentSchedulerVersionNumber;
+FOUNDATION_EXPORT const unsigned char ComponentSchedulerVersionString[];
+
